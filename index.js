1ce18ad45c467a83f44a5db245b03882859ef0e3
Mansi Ayer <42694815+MansiAyer@users.noreply.github.com>
2024-10-30 18:05:49 +0530

    base chat features and base ui

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'chat_struct.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.join("general");
+  socket.broadcast.emit('system message', 'a user connected');
+
+  socket.on('disconnect', () => {
+    socket.leave("general");
+    socket.broadcast.emit('system message', 'user disconnected');
+  });
+
+  socket.on('chat message', (room, msg) => {
+    console.log(room + "," + msg);
+    io.to(room).emit('chat message', room, msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
